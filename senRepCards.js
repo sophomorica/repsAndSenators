ddad4e394f69022ec9fe0b14d654678e046697ef
Patrick Lamoureux <patrick@blackcedar.com>
2019-04-22 19:18:55 -0600

    found 'rept' and deleted it

--- senRepCards.js
+++ senRepCards.js
@@ -15,7 +15,17 @@ const removeCards = () => {
       removeDiv.removeChild(removeDiv.firstChild);
   }
 }
+const createCards = ((personArray)=>{
+  removeCards()
 
+  personArray.forEach((person)=>{
+    let personElement = document.createElement('div')
+    let personName = document.createElement ('p')
+    let imageElement = document.createElement ('img')
+
+
+  })
+})
 
 const democrats = senators.filter(senator => {
   return senator.party === 'D' 
@@ -28,19 +38,25 @@ const simpleReps = representatives.map(rep => {
   return {
     name: `${rep.first_name} ${rep.last_name}`,
     facebook: rep.facebook_account,
-    hompage: rep.url
+    hompage: rep.url,
+    party: rep.party,
+    imagePath: `https://www.congress.gov/img/member/114_rp_${rep.state.toLowerCase()}_${rep.district}_${rep.last_name.toLocaleLowerCase()}_${rep.first_name.toLowerCase()}_200.jpg`, 
 
   }
 })
 
+
+// console.log(simpleReps)
 const simpleSen = senators.map(senator =>{
   return{
     name: `${senator.first_name} ${senator.last_name}`,
     facebook: senator.facebook_account,
-    homepage: senator.url
+    homepage: senator.url,
+    party: senator.party,
+    imagePath: `https://www.congress.gov/img/member/${senator.id.toLowerCase()}.jpg`
   }
 })
-
+// console.log(simpleSen)
 const senWithPics = senators.map(senator =>{
   
   senator.imgURL = `https://www.congress.gov/img/member/${senator.id.toLowerCase()}.jpg`
@@ -48,5 +64,11 @@ const senWithPics = senators.map(senator =>{
 })
 
 const repWithPics = representatives.map(rep=>{
-  rep.imgURL = `https://www.congress.gov/img/member/114_rp_${rept.state.toLowerCase()}_${rep.district}_${rep.last_name.toLocaleLowerCase()}_${rep.first_name.toLowerCase()}_200.jpg`
+  rep.imgURL = `https://www.congress.gov/img/member/114_rp_${rep.state.toLowerCase()}_${rep.district}_${rep.last_name.toLocaleLowerCase()}_${rep.first_name.toLowerCase()}_200.jpg`
 })
+
+
+//variables to make the buttons later
+var allSenators = document.getElementById('senators')
+var allReps = document.getElementById('reps')
+
